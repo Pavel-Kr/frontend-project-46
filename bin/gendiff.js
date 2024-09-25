5cfe150c86123e919e7d4a185090ae319f864220
Pavel Krivosheev <pkrivosheev@bk.ru>
2024-09-25 22:08:52 +0700

    Initial commit

---
+++ bin/gendiff.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import { program } from "commander";
+
+program
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference.')
+  .helpOption('-h, --help', 'output usage information');
+program.parse();
+
+const options = program.opts();
+console.log(program.args);
