6f2ad2192de184f5a7df8055cc08510cee3ebe8f
Pavel Krivosheev <pkrivosheev@bk.ru>
2024-09-25 22:19:29 +0700

    Add arguments and format option for CLI

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,8 +4,12 @@ import { program } from "commander";
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>');
 program.parse();
 
 const options = program.opts();
+console.log(options);
 console.log(program.args);
