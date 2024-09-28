3c8a41a342fab11ba1bdcfe0f0791eb831f4fb9f
Pavel Krivosheev <pkrivosheev@bk.ru>
2024-09-28 22:05:40 +0700

    Refactored parseFile with extname

--- src/parseFile.js
+++ src/parseFile.js
@@ -1,14 +1,15 @@
 import fs from 'fs';
 import YAML from 'yaml';
+import { extname } from 'path';
 
 const parseFile = (filepath) => {
-  const ext = filepath.split('.').at(-1);
+  const ext = extname(filepath);
   const data = String(fs.readFileSync(filepath));
   switch (ext) {
-    case 'json':
+    case '.json':
       return JSON.parse(data);
-    case 'yml':
-    case 'yaml':
+    case '.yml':
+    case '.yaml':
       return YAML.parse(data);
     default:
       throw new Error(`Unsupported file format: ${ext}`);
